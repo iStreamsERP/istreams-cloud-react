9b3090a1705ddd6ee71890b82434fcc81587aa6c
snehadharshini13 <snehadharshini@istreams-erp.com>
2025-06-13 11:54:41 +0530

    2nd update call

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { format, isToday, isYesterday } from "date-fns";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
